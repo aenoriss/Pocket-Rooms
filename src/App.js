98f4961bc3e98dbf7c42aeaa0e2c90fb23c106ec
Aenoris <aenoriss@gmail.com>
2022-03-24 02:49:31 -0300

    asdasdzxcz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useRef, useState, Suspense } from 'react';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import Environment from "./Components/environment";
+import { FirstPersonControls, Sky, DeviceOrientationControls } from '@react-three/drei'
+import Box from "./Box"
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Environment />
+          <ambientLight />
+          <DeviceOrientationControls />
+          <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
+          <pointLight position={[10, 10, 10]} />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
