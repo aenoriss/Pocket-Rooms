a211456997a099dcc17122d46a5d97038dd27f4d
Aenoris <aenoriss@gmail.com>
2022-03-24 23:27:11 -0300

    asdasd2

--- src/App.js
+++ src/App.js
@@ -6,23 +6,31 @@ import * as THREE from 'three';
 import Environment from "./Components/environment";
 import CameraControls from './Components/cameraControls';
 import Avatar from "./Components/avatar";
-import { FirstPersonControls, Sky, DeviceOrientationControls } from '@react-three/drei'
+import { PerspectiveCamera, Sky, DeviceOrientationControls, OrbitControls, FlyControls  } from '@react-three/drei'
 import Box from "./Box"
 
 
 function App() {
+  const [changeCamera, set] = useState(true)
+
   return (
     <div className="App">
       <Canvas>
         <Suspense fallback={null}>
           <Environment />
           <ambientLight />
-          <Avatar/>
-          <CameraControls/>
+
+          <PerspectiveCamera makeDefault={changeCamera} position={[0, 280, 0]}>
+          <FlyControls makeDefault={true}/>
+
+          </PerspectiveCamera  >
+
+
+          <Avatar />
+
+
           <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
           <pointLight position={[10, 10, 10]} />
-          <Box position={[-1.2, 0, 0]} />
-          <Box position={[1.2, 0, 0]} />
         </Suspense>
       </Canvas>
     </div>
---
+++ src/Components/camera.js
@@ -0,0 +1,14 @@
+
+import ReactDOM from 'react-dom';
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { FlyControls, Sky, DeviceOrientationControls, PerspectiveCamera } from '@react-three/drei'
+
+export default function Camera(props) {
+
+    console.log("PROPS", props.position)
+
+    return (
+       null
+    )
+}
