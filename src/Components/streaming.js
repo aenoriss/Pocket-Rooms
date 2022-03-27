f6513ed8ba228ee965d67d49d67dea4adcdbf78b
Aenoris <aenoriss@gmail.com>
2022-03-27 02:33:51 -0300

    zxczcxz

--- src/Components/streaming.js
+++ src/Components/streaming.js
@@ -2,35 +2,24 @@
 import ReactDOM from 'react-dom';
 import React, { useRef, useState } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { Plane } from '@react-three/drei'
+import { Plane, Html } from '@react-three/drei'
 import * as THREE from 'three';
-
+import { Scene } from 'three';
+import '../App.css';
 
 export default function Streaming(props) {
 
-    let video = document.createElement('video');
-    video.width = 320;
-    video.height = 240;
-    video.autoplay = true;
-
-    //Camera Detection
-
-    if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then(function (stream) {
-                video.srcObject = stream;
-                console.log("success")
-            })
-            .catch(function (error) {
-                console.log("Something went wrong!");
-            });
+    function traveler(e){
+        console.log("EVENT", e);
     }
 
-    let videoTexture = new THREE.VideoTexture(video);
-
     return (
-        <Plane args={[100, 60]} position={[20, 300, 100]} rotation={[0, Math.PI , 0]}>
-            <meshBasicMaterial map={videoTexture} opacity={0.5} />
+        <Plane args={[100, 60]} position={[20, 300, 100]} rotation={[0, Math.PI, 0]}>
+            <Html className="content" transform>
+                <div className="wrapper" onclick={"myFunction()"}>
+                    <iframe  width="100%" height="100%"  src="https://www.youtube.com/embed/yTvxE8y1llQ?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </Html>
         </Plane>
     )
 }
